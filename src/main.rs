a5cfa78f6fab7f9e63c5d7d70b3aea27f9b48c37
David Radunz <david@vimtura.com>
2024-04-05 00:10:50 +0700

    Initial commit.

---
+++ src/main.rs
@@ -0,0 +1,72 @@
+#![no_std]
+#![no_main]
+#![feature(type_alias_impl_trait)]
+
+extern crate alloc;
+use core::mem::MaybeUninit;
+use embassy_executor::Spawner;
+use esp_backtrace as _;
+use esp_hal::{clock::ClockControl, peripherals::Peripherals, prelude::*, Delay, embassy};
+use esp_println::println;
+
+use esp_wifi::{initialize, EspWifiInitFor};
+
+use esp_hal::{timer::TimerGroup, Rng};
+#[global_allocator]
+static ALLOCATOR: esp_alloc::EspHeap = esp_alloc::EspHeap::empty();
+
+fn init_heap() {
+    const HEAP_SIZE: usize = 32 * 1024;
+    static mut HEAP: MaybeUninit<[u8; HEAP_SIZE]> = MaybeUninit::uninit();
+
+    unsafe {
+        ALLOCATOR.init(HEAP.as_mut_ptr() as *mut u8, HEAP_SIZE);
+    }
+}
+
+#[main]
+async fn main(spawner: Spawner) {
+    init_heap();
+
+    let peripherals = Peripherals::take();
+    let system = peripherals.SYSTEM.split();
+
+    let clocks = ClockControl::max(system.clock_control).freeze();
+    let mut delay = Delay::new(&clocks);
+
+    // Disable watchdogs
+    // TODO:
+    //let mut rtc = Rtc::new(peripherals.RTC_CNTL);
+    let timer_group0 = TimerGroup::new(peripherals.TIMG0, &clocks);
+    //let mut wdt0 = timer_group0.wdt;
+    let timer_group1 = TimerGroup::new(peripherals.TIMG1, &clocks);
+    //let mut wdt1 = timer_group1.wdt;
+
+    //rtc.swd.disable();
+    //rtc.rwdt.disable();
+    //wdt0.disable();
+    //wdt1.disable();
+
+    // Init embassy
+    embassy::init(&clocks, timer_group0);
+
+    // setup logger
+    // To change the log_level change the env section in .cargo/config.toml
+    // or remove it and set ESP_LOGLEVEL manually before running cargo run
+    // this requires a clean rebuild because of https://github.com/rust-lang/cargo/issues/10358
+    esp_println::logger::init_logger_from_env();
+    log::info!("Logger is setup");
+    println!("Hello world!");
+    let _init = initialize(
+        EspWifiInitFor::Wifi,
+        timer_group1.timer0,
+        Rng::new(peripherals.RNG),
+        system.radio_clock_control,
+        &clocks,
+    )
+    .unwrap();
+    loop {
+        println!("Loop...");
+        delay.delay_ms(500u32);
+    }
+}
